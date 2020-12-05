1cb96349c06ba9aa2b3f30652b0ff7ff031ab30e
Morten Brudvik <morten@brudvik.com>
2020-12-05 14:57:34 +0100

    Removed the dependency on Prism library as the only dependency where on the DelegateCommand, that I instead made an local implementation of.

---
+++ KioskBrowser/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace KioskBrowser
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<object> execute) : this(execute, null) { }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
--- KioskBrowser/MainViewModel.cs
+++ KioskBrowser/MainViewModel.cs
@@ -1,11 +1,10 @@
 ﻿using System.Windows;
-using Prism.Commands;
 
 namespace KioskBrowser
 {
     public class MainViewModel
     {
-        public DelegateCommand CloseWindowCommand => new DelegateCommand(() => { Application.Current.Shutdown(); });
+        public DelegateCommand CloseWindowCommand => new DelegateCommand(x => { Application.Current.Shutdown(); });
 
         public bool IsInstalled => WebView2Install.GetInfo().InstallType != InstallType.NotInstalled;
         public bool IsNotInstalled => !IsInstalled;
